83052ecc91843892cd8ef6b6a4610ba78d2c5179
JFestrella <jestrella@intellisys.com.do>
2019-10-01 08:58:07 -0400

    Challenge 5 complete

--- Challenge5/Challenge5.js
+++ Challenge5/Challenge5.js
@@ -1,28 +1,70 @@
-function client(name, bills) {
-  var Joseph = {
-    name: name,
-    bills: bills,
-    tipCalculate: function() {
-      this.totalTips = [];
-      this.total = [];
-      for (var c = 0; c < this.bills.length; c++) {
-        var per;
-        var bill = this.bills[c];
-        if (bill <= 49) {
-          per = 0.2;
-        } else if (bill > 49 && bill < 200) {
-          per = 0.15;
-        } else {
-          per = 0.1;
-        }
-        this.totalTips[c] = bill * per;
-        this.total[c] = bill * per + bill;
+var joseph = {
+  name: "Joseph Joestar",
+  bills: [182, 84, 42, 24],
+  tipCalculate: function() {
+    this.tips = [];
+    this.finalValues = [];
+    for (var c = 0; c < this.bills.length; c++) {
+      var per;
+      var bill = this.bills[c];
+      if (bill < 50) {
+        per = 0.2;
+      } else if (bill >= 50 && bill < 200) {
+        per = 0.15;
+      } else {
+        per = 0.1;
       }
+      this.tips[c] = bill * per;
+      this.finalValues[c] = bill * per + bill;
     }
-  };
+  }
+};
+var mark = {
+  name: "Markenson Kage",
+  bills: [182, 84, 42, 24, 180],
+  tipCalculate: function() {
+    this.tips = [];
+    this.finalValues = [];
+    for (var c = 0; c < this.bills.length; c++) {
+      var per;
+      var bill = this.bills[c];
+      if (bill < 100) {
+        per = 0.2;
+      } else if (bill >= 100 && bill < 200) {
+        per = 0.1;
+      } else {
+        per = 0.25;
+      }
+      this.tips[c] = bill * per;
+      this.finalValues[c] = bill * per + bill;
+    }
+  }
+};
 
-  Joseph.tipCalculate();
-  console.log(Joseph);
+var calAcount = function(tips) {
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++) {
+    sum = sum + tips[i];
+  }
+  return sum / tips.length;
+};
+
+joseph.tipCalculate();
+mark.tipCalculate();
+joseph.average = calAcount(joseph.tips);
+mark.average = calAcount(mark.tips);
+console.log(joseph, mark);
+
+if (joseph.average > mark.average) {
+  console.log(
+    joseph.name +
+      "'s family pays higher tips, with an average of $" +
+      joseph.average
+  );
+} else if (mark.average > joseph.average) {
+  console.log(
+    mark.name +
+      "'s family pays higher tips, with an average of $" +
+      mark.average
+  );
 }
-
-client("Joseph Joestar", [192, 30, 50, 20]);
